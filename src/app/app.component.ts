1863f376542c478089223d8b811e47378e6979d6
eastcoastdeveloper <uxdeveloper@protonmail.com>
2021-02-25 10:01:23 -0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,44 @@
-import { Component, Injectable, VERSION } from '@angular/core';
+import { Component, Injectable, VERSION } from "@angular/core";
 
 // Get Window Size, step 1
-function _window():any {
+function _window(): any {
   return window;
 }
 
 // Get Window Size, step 2
 @Injectable()
-  export class WindowRef {
-    get nativeWindow():any {
-      return _window();
-    }
+export class WindowRef {
+  get nativeWindow(): any {
+    return _window();
   }
+}
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ],
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
   host: {
-    "(window:resize)":"onWindowResize($event)"
+    "(window:resize)": "onWindowResize($event)"
   }
 })
-export class AppComponent  {
-  
+export class AppComponent {
   // Get Window Size, step 3
   windowSize: any;
-  isMobile :boolean = false;
-  width:number = window.innerWidth;
-  height:number = window.innerWidth;
-  mobileWidth:number = 760;
+  isMobile: boolean = false;
+  width: number = window.innerWidth;
+  height: number = window.innerWidth;
+  mobileWidth: number = 760;
 
-  ngOnInit():void {
+  ngOnInit(): void {
     // Get Window Size, step 4
     this.isMobile = this.width < this.mobileWidth;
   }
 
   // Get Window Size, step 5
-  onWindowResize(event){
+  onWindowResize(event) {
     this.width = event.target.innerWidth;
     this.height = event.target.innerHeight;
     this.isMobile = this.width < this.mobileWidth;
-    console.log(this.width, this.isMobile)
+    console.log(this.width, this.isMobile);
   }
-
 }
-
