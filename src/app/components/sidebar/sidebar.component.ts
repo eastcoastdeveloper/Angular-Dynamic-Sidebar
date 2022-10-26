cd4129f32dc5c5cd8d7bbb14af8ea02e87505116
eastcoastdeveloper <58962475+eastcoastdeveloper@users.noreply.github.com>
2022-10-26 12:21:41 -0700

    1 file modified, 1 deleted

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import * as data from './sidebar.json';
 import { SidebarModel } from './sidebar.model';
 
 @Component({
@@ -9,19 +8,12 @@ import { SidebarModel } from './sidebar.model';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
-  data: any = data;
   result: SidebarModel[] = [];
 
   constructor(private _http: HttpClient) {
-    for (let key in data.navitems) {
-      if (data.navitems.hasOwnProperty(key)) {
-        this.result.push(data.navitems[key]);
-      }
-    }
-
     this._http.get<SidebarModel[]>('assets/sidebar.json').subscribe((res) => {
       this.result = res;
-      console.log(this.result)
+      console.log(this.result);
     });
   }
 
