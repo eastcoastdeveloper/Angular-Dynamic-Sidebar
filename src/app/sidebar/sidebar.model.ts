cae7698b9f44b2300ee149130303632280f6a1cb
eastcoastdeveloper <58962475+eastcoastdeveloper@users.noreply.github.com>
2023-08-24 22:53:44 -0700

    1 file modified

--- src/app/sidebar/sidebar.model.ts
+++ src/app/sidebar/sidebar.model.ts
@@ -2,5 +2,5 @@ export class SidebarModel {
   linkText: string;
   parentLink: string;
   menu: boolean;
-  submenu: { childtext: string; link: string }[];
+  submenu: { childtext: string; link: string }[] | null;
 }
